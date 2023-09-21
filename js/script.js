423f765befd9130bc093efd2e6fa6f4664e9e627
Essel Luna <essel.luna1@trans-cosmos.co.jp>
2023-09-21 16:18:18 +0800

    finalize index css and js file

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ var thankYou = document.querySelector(".thankYou");
 var form = document.getElementById("ratingForm");
 
 document.getElementById("ratingForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
+  event.preventDefault(); // Prevent form submission
   
   // Get the selected rating value
   var selector = "input[name=rate]:checked";
   var checkedInput = document.querySelector(selector);
 
-  // Perform any necessary processing or validation here
+  // form validation 
   if(checkedInput !== null){
     var selectedRating = document.querySelector(".rateTxt");
     selectedRating.textContent = "You selected " + checkedInput.getAttribute("value") + " out of 5"
@@ -17,21 +17,3 @@ document.getElementById("ratingForm").addEventListener("submit", function(event)
     thankYou.classList.remove("hidden");
   }
 });
-
-// var rateStart = document.querySelector(".rating");
-// var thankYou = document.querySelector(".thankYou");
-// var form = document.getElementById("ratingForm");
-
-// form.addEventListener("submit", function(event){
-//     event.preventDefault(); //prevents Browser from refreshing
-//     var selector = "input[name=rate]:checked";
-//     var checkedInput = document.querySelector(selector);
-        
-//     // User needs to select an Option in order to go on
-//     if(checkedInput !== null){
-//         var selectedRating = document.querySelector(".rateTxt");
-//         selectedRating.textContent = "You selected " + checkedInput.getAttribute("value") + " out of 5"
-//         rateStart.classList.add("hidden");
-//         thankYou.classList.remove("hidden");
-//     }
-// })
